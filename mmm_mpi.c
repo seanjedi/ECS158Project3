9de7defa61d78290d5c674ec6ccfc08319f54dbf
Sean Patrick Malloy <spmalloy@ucdavis.edu>
2018-11-11 15:16:32 -0800

    V.0.4

--- mmm_mpi.c
+++ mmm_mpi.c
@@ -2,14 +2,15 @@
 #include<stdlib.h>
 #include<time.h>
 #include<mpi.h>
+#include<string.h>
 
 unsigned int matrix_checksum(int N, void *M, unsigned int size);
 
 /////////////////////
 //Multiply Function//
 /////////////////////
-void multiply_mpi(double* A, double* B, double* C, int N, int chunk, int myChunk, int rank){
-	int start = chunk * rank;
+void multiply_mpi(double* A, double* B, double* C, int N, int chunk_size, int myChunk, int rank){
+	int start = chunk_size * rank;
 	int stop = start + myChunk;
 
 	for(int i = start ; i < stop; i++){
@@ -29,7 +30,7 @@ int main(int argc, char **argv)
 {
 	//MPI Intialization//
 	MPI_Init(&argc, &argv);
-	int com_size, chunk, com_rank, chunk_size, last_chunk, my_chunk;
+	int com_size, com_rank, chunk_size, last_chunk, my_chunk;
 	MPI_Comm_size(MPI_COMM_WORLD, &com_size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &com_rank);
 
@@ -50,7 +51,6 @@ int main(int argc, char **argv)
 	}
 	
 		chunk_size = N/(com_size);
-		chunk = chunk_size;
 		last_chunk = N - (chunk_size * (com_size - 1 ));
 
 		if(com_rank == com_size - 1)
@@ -68,6 +68,7 @@ int main(int argc, char **argv)
 	double* A = malloc(sizeof(double) * N * N);
 	double* B = malloc(sizeof(double) * N * N);
 	double* C = malloc(sizeof(double) * N * N);
+	double* temp = malloc(sizeof(double) * N * N);
 	for(int i = 0; i < N * N; i++){ // Matrix initialization
 		A[i] = i/N + i%N;
 		B[i] = i/N + (i%N)*2;
@@ -85,20 +86,26 @@ int main(int argc, char **argv)
 		if(com_rank == 0)
 			multiply_mpi(A, B, C, N, N, N, 0);
 	}else{
-		multiply_mpi(A, B, C, N, chunk, my_chunk, com_rank);
+		multiply_mpi(A, B, temp, N, chunk_size, my_chunk, com_rank);
 
-		//Receive other data
+		//Receive matrix data
 		if(com_rank == 0){
+			for(int i = 0; i < N*N; i++)
+				C[i] += temp[i];
 			for(int i = 1; i < com_size; i++){
 				if(i  == com_size - 1)
-					MPI_Recv(C, last_chunk, MPI_DOUBLE, i, MPI_ANY_TAG, MPI_COMM_WORLD, 0);
+					MPI_Recv((temp + chunk_size * i), last_chunk * last_chunk, MPI_DOUBLE, i, MPI_ANY_TAG, MPI_COMM_WORLD, 0);
 				else
-					MPI_Recv(C, chunk, MPI_DOUBLE, i, MPI_ANY_TAG, MPI_COMM_WORLD, 0);
+					MPI_Recv((temp + chunk_size * i), chunk_size * chunk_size, MPI_DOUBLE, i, MPI_ANY_TAG, MPI_COMM_WORLD, 0);
+				for(int i = 0; i < N*N; i++)
+					C[i] += temp[i];
+				// printf("C: %u\n", matrix_checksum(N, C, sizeof(double)));
+				// printf("temp: %u\n", matrix_checksum(N, temp, sizeof(double)));
 				
 			}
 		}else{
-			//Send other data
-			MPI_Send(C, chunk, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+			//Send matrix data
+			MPI_Send((temp + chunk_size * com_rank), my_chunk * my_chunk, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
 		}
 	}
 
@@ -116,10 +123,10 @@ int main(int argc, char **argv)
 		printf("C: %u\n", matrix_checksum(N, C, sizeof(double)));
 	}
 
-	MPI_Finalize();
-    free(A);
+	free(A);
     free(B);
     free(C);	
+	MPI_Finalize();
 	
 	return 0;
 }
