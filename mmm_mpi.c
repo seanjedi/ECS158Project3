15cc1d73959f9b992376e47da73f38f663bb2810
Sean Patrick Malloy <spmalloy@ucdavis.edu>
2018-11-11 14:14:33 -0800

    Changes to mpi.c

--- mmm_mpi.c
+++ mmm_mpi.c
@@ -8,17 +8,19 @@ unsigned int matrix_checksum(int N, void *M, unsigned int size);
 /////////////////////
 //Multiply Function//
 /////////////////////
-void multiply_mpi(double* A, double* B, double* C, int N, int chunk, int rank){
+void multiply_mpi(double* A, double* B, double* C, int N, int chunk, int myChunk, int rank){
 	int start = chunk * rank;
-	int stop = start + chunk;
-	
-	for(int i = start; i < stop; i++){
-		for(int j = start; j < stop; j++){
-			for(int k = start; k < stop; k++){
-				C[i*N + j] += A[i*N + k] * B[k*N + j];
-			}
-		}
-	}
+	int stop = start + myChunk;
+
+	for(int i = start ; i < stop; i++){
+        	for(int k = 0; k < N; k++){
+	                double temp = A[i*N + k];
+                        for(int j = 0; j < N; j++){
+          	              C[i*N + j] += temp * B[k*N + j];
+                        }
+                }
+    }
+
 	return;
 }
 
@@ -27,7 +29,7 @@ int main(int argc, char **argv)
 {
 	//MPI Intialization//
 	MPI_Init(&argc, &argv);
-	int com_size, chunk, com_rank, chunk_size, last_chunk;
+	int com_size, chunk, com_rank, chunk_size, last_chunk, my_chunk;
 	MPI_Comm_size(MPI_COMM_WORLD, &com_size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &com_rank);
 
@@ -47,40 +49,51 @@ int main(int argc, char **argv)
                 exit(1);
 	}
 	
-		chunk_size = N/(com_size - 1);
-		last_chunk = N - ((com_size - 1) * chunk_size);
+		chunk_size = N/(com_size);
+		chunk = chunk_size;
+		last_chunk = N - (chunk_size * (com_size - 1 ));
 
 		if(com_rank == com_size - 1)
-			chunk = last_chunk;
+			my_chunk = last_chunk;
 		else
-			chunk = chunk_size;
+			my_chunk = chunk_size;
 
-	double* A = malloc(sizeof(double) * chunk * chunk);
-	double* B = malloc(sizeof(double) * chunk * chunk);
-	double* C = malloc(sizeof(double) * chunk * chunk);
-	for(int i = 0; i < chunk * chunk; i++){ // Matrix initialization
-		A[i] = i/(chunk * com_rank) + i % (chunk * com_rank);
-		B[i] = i/(chunk * com_rank) + (i % (chunk * com_rank)) *2;
+	// double* A = malloc(sizeof(double) * chunk * chunk);
+	// double* B = malloc(sizeof(double) * chunk * chunk);
+	// double* C = malloc(sizeof(double) * chunk * chunk);
+	// for(int i = 0; i < chunk * chunk; i++){ // Matrix initialization
+	// 	A[i] = i/(chunk * com_rank) + i % (chunk * com_rank);
+	// 	B[i] = i/(chunk * com_rank) + (i % (chunk * com_rank)) *2;
+	// }
+	double* A = malloc(sizeof(double) * N * N);
+	double* B = malloc(sizeof(double) * N * N);
+	double* C = malloc(sizeof(double) * N * N);
+	for(int i = 0; i < N * N; i++){ // Matrix initialization
+		A[i] = i/N + i%N;
+		B[i] = i/N + (i%N)*2;
 	}
 
 	struct timespec before, after;
 
 	if(com_rank == 0){
-		double* C = malloc(sizeof(double) * N * N);
+		// double* C = malloc(sizeof(double) * N * N);
 		clock_gettime(CLOCK_MONOTONIC, &before);
 	}
 	
 	//Implement MPI code//
 	if(N < com_size){
 		if(com_rank == 0)
-			multiply_mpi(A, B, C, N, N, 0);
+			multiply_mpi(A, B, C, N, N, N, 0);
 	}else{
-		multiply_mpi(A, B, C, N, chunk, com_rank);
+		multiply_mpi(A, B, C, N, chunk, my_chunk, com_rank);
 
 		//Receive other data
 		if(com_rank == 0){
 			for(int i = 1; i < com_size; i++){
-				MPI_Recv(C, chunk, MPI_DOUBLE, i, MPI_ANY_TAG, MPI_COMM_WORLD, 0);
+				if(i  == com_size - 1)
+					MPI_Recv(C, last_chunk, MPI_DOUBLE, i, MPI_ANY_TAG, MPI_COMM_WORLD, 0);
+				else
+					MPI_Recv(C, chunk, MPI_DOUBLE, i, MPI_ANY_TAG, MPI_COMM_WORLD, 0);
 				
 			}
 		}else{
