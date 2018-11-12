eb12adce3ea61773109d7cb9060742c6dc68384d
Sean Patrick Malloy <spmalloy@ucdavis.edu>
2018-11-12 13:46:07 -0800

    V.1.0
    Got mmm_mpi.c to work as intended on the guidelines

--- mmm_mpi.c
+++ mmm_mpi.c
@@ -5,7 +5,6 @@
 #include<string.h>
 
 unsigned int matrix_checksum(int N, void *M, unsigned int size);
-
 /////////////////////
 //Multiply Function//
 /////////////////////
@@ -21,8 +20,132 @@ void multiply_mpi(double* A, double* B, double* C, int N, int chunk_size, int my
                         }
                 }
     }
-
 	return;
+}
+
+void com0(int com_rank, int com_size, int argc, char **argv){
+	if(argc != 2){//if wrong number of inputs
+		fprintf(stderr, "Usage: %s N\n", *argv);
+		// MPI_Abort(MPI_COMM_WORLD, 1);
+		exit(1);
+	}
+
+	int N, chunk_size;
+
+	if(!(N = atoi(argv[1]))){
+		fprintf(stderr, "Error: wrong matrix order (N > 0)\n");
+		exit(-1);
+		// MPI_Abort(MPI_COMM_WORLD, 1);
+
+	}
+	if(N <= 0){
+		fprintf(stderr, "Error: wrong matrix order (N > 0)\n");
+		exit(-1);
+		// MPI_Abort(MPI_COMM_WORLD, 1);
+	}
+	struct timespec before, after;
+	clock_gettime(CLOCK_MONOTONIC, &before);
+	MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+	chunk_size = N/(com_size);
+	double* A = malloc(sizeof(double) * N * N);
+	double* B = malloc(sizeof(double) * N * N);
+	double* C = malloc(sizeof(double) * N * N);
+	for(int i = 0; i < N * N; i++){ // Matrix initialization
+		A[i] = i/N + i%N;
+		B[i] = i/N + (i%N)*2;
+		C[i] = 0;
+	}
+
+	if(N < com_size)
+		multiply_mpi(A, B, C, N, N, N, 0);
+	else{
+		
+		
+		multiply_mpi(A, B, C, N, chunk_size, chunk_size, com_rank);
+
+		// indexing for our C
+		int offset = com_rank+chunk_size*N;
+		int last_chunk = N - (chunk_size * (com_size - 1 ));
+
+		for(int i = 1; i < com_size; i++){
+			if(i  == com_size - 1) {
+				double *buffer = (double*) calloc(N*last_chunk, sizeof(double));
+
+				// using buffer instead of temp because MPI_Recv kept on receiving data from index 0 
+				// regardless of how I tried to index the location, it would always receive the data at 0
+				// I may be wrong about this, but hey its working.
+				MPI_Recv(&buffer[0], N*last_chunk, MPI_DOUBLE, i, MPI_ANY_TAG, MPI_COMM_WORLD, 0);
+					
+				int count = 0;
+				for(int i = offset; i < offset+N*last_chunk; i++) {
+					C[i] += buffer[count];
+					count++;
+				}
+				offset += N*last_chunk;
+				free(buffer);
+			} else {
+				double *buffer = (double*) malloc(sizeof(double)*N*chunk_size);
+				for(int i = 0; i < N*chunk_size; i++) {
+					buffer[i] = 0.0;
+				}
+					
+				MPI_Recv(&buffer[0], N*chunk_size, MPI_DOUBLE, i, MPI_ANY_TAG, MPI_COMM_WORLD, 0);
+
+				int count = 0;
+				for(int i = offset; i < offset+N*chunk_size; i++) {
+					C[i] += buffer[count];
+					count++;
+				}
+				offset += N*chunk_size;
+				free(buffer);
+			}
+		}
+
+		clock_gettime(CLOCK_MONOTONIC, &after);
+		unsigned long elapsed_ns = (after.tv_sec - before.tv_sec)*(1E9) + after.tv_nsec - before.tv_nsec;
+		double seconds = elapsed_ns / (1E9);
+		printf("Running time: %f secs\n", seconds);
+
+		printf("A: %u\n", matrix_checksum(N, A, sizeof(double)));
+		printf("B: %u\n", matrix_checksum(N, B, sizeof(double)));
+		printf("C: %u\n", matrix_checksum(N, C, sizeof(double)));
+		free(A);
+    	free(B);
+    	free(C);
+
+
+	}
+}
+
+void coms(int com_rank, int com_size){
+	int N, my_chunk, chunk_size;
+	
+	MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+	if(N > com_size){
+		chunk_size = N/(com_size);
+		if(com_rank == com_size - 1)
+			my_chunk = N - (chunk_size * (com_size - 1 ));
+		else
+			my_chunk = N/(com_size);
+
+		double* A = malloc(sizeof(double) * N * N);
+		double* B = malloc(sizeof(double) * N * N);
+		double* C = malloc(sizeof(double) * N * N);
+		// start = chunk_size * com_rank;
+		for(int i = 0; i < N * N; i++){ // Matrix initialization
+			A[i] = i/N + i%N;
+			B[i] = i/N + (i%N)*2;
+			C[i] = 0;
+		}
+
+		multiply_mpi(A, B, C, N, chunk_size, my_chunk, com_rank);
+		MPI_Send((C+(N*com_rank*chunk_size)), my_chunk*N, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+		free(A);
+    	free(B);
+    	free(C);
+	}
 }
 
 
@@ -30,101 +153,15 @@ int main(int argc, char **argv)
 {
 	//MPI Intialization//
 	MPI_Init(&argc, &argv);
-	int com_size, com_rank, chunk_size, last_chunk, my_chunk;
+	int com_size, com_rank;
 	MPI_Comm_size(MPI_COMM_WORLD, &com_size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &com_rank);
 
-	if(argc != 2){//if wrong number of inputs
-		fprintf(stderr, "Usage: %s N\n", *argv);
-		exit(1);
-	}
-
-	int N;
-
-	if(!(N = atoi(argv[1]))){
-		printf("Error: wrong matrix order (0 < N <= 2000)\n");
-		exit(1);
-	}
-	if(N <= 0 || N > 2000){
-                printf("Error: wrong matrix order (0 < N <= 2000)\n");
-                exit(1);
-	}
-	
-		chunk_size = N/(com_size);
-		last_chunk = N - (chunk_size * (com_size - 1 ));
-
-		if(com_rank == com_size - 1)
-			my_chunk = last_chunk;
-		else
-			my_chunk = chunk_size;
-
-	// double* A = malloc(sizeof(double) * chunk * chunk);
-	// double* B = malloc(sizeof(double) * chunk * chunk);
-	// double* C = malloc(sizeof(double) * chunk * chunk);
-	// for(int i = 0; i < chunk * chunk; i++){ // Matrix initialization
-	// 	A[i] = i/(chunk * com_rank) + i % (chunk * com_rank);
-	// 	B[i] = i/(chunk * com_rank) + (i % (chunk * com_rank)) *2;
-	// }
-	double* A = malloc(sizeof(double) * N * N);
-	double* B = malloc(sizeof(double) * N * N);
-	double* C = malloc(sizeof(double) * N * N);
-	double* temp = malloc(sizeof(double) * N * N);
-	for(int i = 0; i < N * N; i++){ // Matrix initialization
-		A[i] = i/N + i%N;
-		B[i] = i/N + (i%N)*2;
-		C[i] = 0;
-		temp[i] = 0;
-	}
-
-	struct timespec before, after;
-
-	if(com_rank == 0){
-		// double* C = malloc(sizeof(double) * N * N);
-		clock_gettime(CLOCK_MONOTONIC, &before);
-	}
-	
-	//Implement MPI code//
-	if(N < com_size){
-		if(com_rank == 0)
-			multiply_mpi(A, B, C, N, N, N, 0);
-	}else{
-		multiply_mpi(A, B, temp, N, chunk_size, my_chunk, com_rank);
-
-		//Receive matrix data
-		if(com_rank == 0){
-			for(int i = 0; i < N*N; i++)
-				C[i] += temp[i];
-			for(int i = 1; i < com_size; i++){
-				MPI_Recv(temp, N*N, MPI_DOUBLE, i, MPI_ANY_TAG, MPI_COMM_WORLD, 0);
-				for(int i = 0; i < N*N; i++)
-					C[i] += temp[i];
-				// printf("C: %u\n", matrix_checksum(N, C, sizeof(double)));
-				// printf("temp: %u\n", matrix_checksum(N, temp, sizeof(double)));
-				
-			}
-		}else{
-			//Send matrix data
-			MPI_Send(temp, N * N, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
-		}
-	}
-
-	//End MPI Code 
-	if(com_rank == 0){
-		clock_gettime(CLOCK_MONOTONIC, &after);
-
-		unsigned long elapsed_ns = (after.tv_sec - before.tv_sec)*(1E9) + after.tv_nsec - before.tv_nsec;
-		double seconds = elapsed_ns / (1E9);
-
-		printf("Running time: %f secs\n", seconds);
-
-		printf("A: %u\n", matrix_checksum(N, A, sizeof(double)));
-		printf("B: %u\n", matrix_checksum(N, B, sizeof(double)));
-		printf("C: %u\n", matrix_checksum(N, C, sizeof(double)));
-	}
-
-	free(A);
-    free(B);
-    free(C);	
+	if(com_rank == 0)
+        com0(com_rank, com_size, argc, argv);
+	else
+        coms(com_rank, com_size);
+		
 	MPI_Finalize();
 	
 	return 0;
